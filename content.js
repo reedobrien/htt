af625f5f93a53b7b03a4f1660a49743603f2d0ec
Ben Jeavons <jeavons@gmail.com>
2013-11-09 17:23:16 -0500

    writeDialog with jquery-ui and form.submit handler

--- content.js
+++ content.js
@@ -1,23 +1,56 @@
 $( document ).ready(function() {
 
-  $("form").submit(function (e) {
-    e.preventDefault(); // disables form submit from continueing
+  //$("form").append("<div id='dialog' title='dialog'/>");
 
-	var forminfo = "";
-	
+  /*
+    Write HTML string to page.
+   */
+  writeDialog = function(html, form) {
+    open = '<div style="background-color: white; border-style: solid; border-width: 10px; border-color: rgb(51, 0, 51) !important; background-position: initial initial !important; background-repeat: initial initial !important;">';
+    close = '</div>';
+    //$('body').prepend(open + html + close);
+
+    $('<div style="border-style: solid; border-width: 10px; border-color: rgb(51, 0, 51) !important; background-position: initial initial !important; background-repeat: initial initial !important"></div>').appendTo('body')
+      .html('<div><h6>' + html + '</h6></div>')
+      .dialog({
+          modal: true, title: 'message', zIndex: 10000, autoOpen: true,
+          width: 'auto', resizable: false,
+          buttons: {
+              Yes: function () {
+                  $(this).dialog("close");
+                  formSubmit(form);
+              },
+              No: function () {
+                  console.log('no');
+                  $(this).dialog("close");
+              }
+          },
+          close: function (event, ui) {
+              $(this).remove();
+          }
+    });
+  }
+
+  formSubmit = function(form) {
+    form.submit();
+  }
+
+  $("form").submit(function (e) {
+    // Disable form submit from continuing until dialog allows
+    e.preventDefault();
+
+    var forminfo = "";
+
     $(this).find(':text').each(function() {
-	  forminfo = forminfo.concat($(this).attr("name")) + ": " + $(this).val() + "\n"; 
+	  forminfo = forminfo.concat($(this).attr("name")) + ": " + $(this).val() + "\n";
       console.log($(this).attr('name'));
     });
-	alert(forminfo);
 
-    /*inputs = $(this, ':input');
-    console.log(inputs.count);
-        var values = {};
-        inputs.each(function() {
-          console.log($(this).val());
-            values[this.name] = $(this).val();
-        });*/
+    writeDialog(forminfo, this);
 
   });
+
+
 });
+
+
