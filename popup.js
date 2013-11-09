fd30bc94398e242ad02485f748a853c2c856b693
Reed O'Brien <reed@reedobrien.com>
2013-11-09 13:50:22 -0500

    updated with some basic chack (bad logic)

--- popup.js
+++ popup.js
@@ -2,4 +2,29 @@
 // All rights reserved. Use of this source code is governed by a 
 // BSD-style license that can be found in the LICENSE file.
 
-// JS Here
+// First Check if 
+
+var autofillEnabled
+
+chrome.privacy.services.autofillEnabled.get({}, function(details) {
+  if (details.value) {
+    autofillEnabled=true;
+    console.log("It is enabled");  
+  }
+  else {
+    autofillEnabled=false;
+    console.log("It is NOT enabled");
+  }
+});
+
+
+$( document ).ready(function() {
+  console.log(autofillEnabled);
+  if (autofillEnabled===true) {
+      $("#autofill-enabled").html("AutoFill is enabled");
+    }
+  else
+  { 
+    $("#autofill-enabled").html("AutoFill is disabled");
+  }
+}); 
